0a3747ab4bf791fcc9b5328ae0675a9734411eaa
Tamasha <tamasha.14@cse.mrt.ac.lk>
2017-02-22 22:35:05 +0530

    login and navbar

--- TableLock_app/config/routes.rb
+++ TableLock_app/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'login/index'
+  root 'login#index'
+
+  get 'welcome/index'
+
+  root 'welcome#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
